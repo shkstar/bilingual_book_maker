43a1f69613b11c6e6ae9906f4dceeafbf4ac71d1
shkstar <10355506d@connect.polyu.hk>
2023-09-08 15:17:08 +0800

    Update make_book.py
    Add claude

--- make_book.py
+++ make_book.py
@@ -1,4 +1,12 @@
+from litellm import completion
 from book_maker.cli import main
 
 if __name__ == "__main__":
-    main()
+    
+  # Initialize translator
+  translate_model = completion
+
+  # Call completion with Claude  
+  response = completion(model="claude-2", messages=messages)
+
+  main(model="claude")
